62dc441be414d8450a6d3325b4d5c84712a0ebae
pawelek <p.dawiduk@gmail.com>
2016-07-06 22:34:17 +0200

    stworzenie Repeat Rule

--- src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,19 +1,23 @@
 package edu.iis.mto.multithread;
 
+
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.concurrent.Executor;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by pawelek on 06.07.16.
  */
 public class BetterRadarTest {
 
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
     @Test
+    @RepeatRule.Repeat(times = 10)
     public void launchPatriotOnceWhenNoticesAScudMissle() {
         PatriotBattery patriotBattery = mock(PatriotBattery.class);
         Executor executor = new Executor() {
@@ -26,6 +30,8 @@ public class BetterRadarTest {
         BetterRadar betterRadar = new BetterRadar(patriotBattery, executor);
         betterRadar.notice(new Scud());
         verify(patriotBattery, times(10)).launchPatriot();
+
+
     }
 
 }
